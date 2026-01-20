66ee22febd86ee58944e11848d70258c48db43cc
lailatfra <lailatulfitria6619@gmail.com>
2026-01-20 10:17:38 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/lailatfra/go-say-hello
+module github.com/lailatfra/go-say-hello/v2
 
 go 1.25.5
--- say_hello.go
+++ say_hello.go
@@ -1,6 +1,6 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello Peach"
+func SayHello(name string) string {
+	return "Hello Peach" + name
 }
 
